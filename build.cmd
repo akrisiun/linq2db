f26c5086f5874f822ddfc3814b78344bbe7197d2
AndriusK <andrius.k@sanitex.eu>
2016-07-11 18:29:22 +0300

    Smaller git repo, build.cmd

---
+++ build.cmd
@@ -0,0 +1,13 @@
+@set msbuild="%ProgramFiles%\msbuild\14.0\Bin\MSBuild.exe"
+@if not exist %msbuild% @set msbuild="%ProgramFiles%\MSBuild\14.0\Bin\MSBuild.exe"
+@if not exist %msbuild% @set msbuild="%ProgramFiles(x86)%\MSBuild\12.0\Bin\MSBuild.exe"
+@if not exist %msbuild% @set msbuild="%ProgramFiles%\MSBuild\12.0\Bin\MSBuild.exe"
+
+nuget restore linq2db.sln
+@REM cd src\
+@REM dotnet restore
+@REM cd ..\
+
+%msbuild% linq2db.sln /v:m /m
+
+@PAUSE
